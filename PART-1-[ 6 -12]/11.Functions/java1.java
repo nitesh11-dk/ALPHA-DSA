d442eeba96fbca389fda7cc25cc23b44e50c9f5f
Nitesh-DK <nittesh.dk@gmail.com>
2024-09-26 19:26:57 +0530

    all checking done

--- PART-1-[ 6 -12]/11.Functions/java1.java
+++ PART-1-[ 6 -12]/11.Functions/java1.java
@@ -1,7 +1,5 @@
 import java.util.*;
-
 public class java1{
-
 
 
 
@@ -9,8 +7,8 @@ public class java1{
 
 public static void main(String args[]){
 Scanner scnr = new Scanner(System.in);
-// long n = scnr.nextLong(); 
-// long r = scnr.nextLong(); 
+// long n = scnr.nextLong();
+// long r = scnr.nextLong();
 
 
 // System.out.println(binoCoff(n ,r));
@@ -41,7 +39,7 @@ public static long factorial(long a) {
 }
 
 
-// function overloading depends only on parameter , like type of paramter and no of parameters onlly 
+// function overloading depends only on parameter , like type of paramter and no of parameters onlly
 
 public static int sum(int  a , int  b){
 
@@ -71,15 +69,15 @@ return true  ;
 
 
 
-// public static --> access modifier -- in loop 
-//  vo function jo hum classes ye yander likh hai yuse hum methods kahate hai 
+// public static --> access modifier -- in loop
+//  vo function jo hum classes ye yander likh hai yuse hum methods kahate hai
 
 
-//  parameter is what when you create while creating the function is called formal parameter of parameter 
+//  parameter is what when you create while creating the function is called formal parameter of parameter
 
-//  argument is what when you call the function , and what values you pass with the fcuntion is called as actual parameter or argument 
-// java me hamesa  call by value diya jata hai function me  
+//  argument is what when you call the function , and what values you pass with the fcuntion is called as actual parameter or argument
+// java me hamesa  call by value diya jata hai function me
 
 
-//  function overolading 
-// me same class there are   multiple function with the same name but with different parameter 
+//  function overolading
+// me same class there are   multiple function with the same name but with different parameter
