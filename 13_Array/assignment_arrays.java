1f28ab579665b8b27505e1d4e6a92eed964345ef
Nitesh-DK <nittesh.dk@gmail.com>
2024-10-06 21:39:07 +0530

    assignment question done

--- 13_Array/assignment_arrays.java
+++ 13_Array/assignment_arrays.java
@@ -1,12 +1,9 @@
 public class assignment_arrays {
-
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) {       
 
 // ?         Question 1: Given an integer array nums, return true if any value appears at least twice in the  array, and return false if every element is distinct.
 // if  duplicate --> true , not false 
 // System.out.print(Duplicate_Num(new int[]{2,3,4,5,}));
-
 
 // ? Question 2: There is an integer array nums sorted in ascending order (with distinct values). Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
 // *  Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums. You must write an algorithm with O(log n) runtime complexity.
@@ -14,7 +11,6 @@ public class assignment_arrays {
 // Haan, bilkul sahi samjha! Tumhe sirf yeh check karna hai ki nums array mein target value exist karti hai ya nahi. Tumhe rotate operation nahi karna hai; bas check karna hai ki agar target array mein hai, toh uska index return karo. Agar target nahi hai, toh -1 return karo.
 // Isko efficient tarike se O(log n) time complexity mein karna hoga,
 // System.out.println(TargetExist(new int []{2,3,4,5,6}, 11));
-
 
 
 // The question is as follows:
