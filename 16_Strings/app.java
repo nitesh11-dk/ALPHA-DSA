c4b1b23c65f17107d922e571bcb41fa28cfc4482
Nitesh-DK <nittesh.dk@gmail.com>
2024-10-19 19:30:14 +0530

    some string questions and stringbuilder done

--- 16_Strings/app.java
+++ 16_Strings/app.java
@@ -38,9 +38,95 @@ String strconcat = str + str2;
 // System.out.print(isPalindrome("madam"));
 //  T 0(n)
 
+// ? Question 2
+// Given a route containing 4 directions (E, W, N, S),
+// find the shortest path to reach destination.
+//  Math.sqrt retrun a type double so make sure you do type casting 
+// System.out.print(getShortestDist("EEENNNN"));
+
+
+// ? comparsion 
+// System.out.print("Hello" == "Hello");
+String s1 = "Tony";
+String s2 = "Tony";
+String s3 = new String("Tony");
+
+// System.out.println(s1 ==s2); // true --> because when we are decelaring this two string withour new keyword so both the string is pointing towarech each other 
+// System.out.println(s1 ==s3); // false  -- > here it is false due to new keyword so the adress of both of the string is differnt 
+//  the concept used is interning 
+//  == checks at object level to compare the values and not the address
+//  so we use method as equals
+
+// System.out.println(s1.equals(s3)); // now it is comparing the values  not the adress of the obejct 
+
+
+// ? Substring 
+//  continue part of the string 
+//  System.out.println(str.substring(1,4)); // here 3 is excluded 
+
+//  *  Question 3
+// For a given set of Strings, print the largest string.
+// "apple", "mango", "banana"
+//  lexicographically , like  alphabetical order
+
+//  compareTo --> A != a
+//   compareToIgnoreCase --> A == a
+
+// printLargest(new String[]{"apple", "mango", "banana"});
+//  T O (n * m)
+
+
+//  ? Interning
+
+// ? due to immutablity nature os string there is a differnt data structure caleeed stringbuilder
+StringBuilder sb = new StringBuilder("Hello ");
+System.out.println(sb);
+System.out.println(sb.append("World"));
+for (char c= 'a' ; c<='z' ; c++){
+    sb.append(c);
+}
+    System.out.println(sb); // TO(n) , if you are using a string here then the time complexity will be O(n *n)
+    // System.out.print(sb.length());
+    
+
+// we can use .tostring methods on objects , just rememeber on object  it means it will  only work for Integer not for int okay 
+
+
+
 
     }
 
+public static void printLargest(String []fruits){
+
+String largest = fruits[0];
+for(int i = 0 ; i<fruits.length ; i++){ 
+if(largest.compareTo(fruits[i])<0){
+    largest = fruits[i];
+
+}
+}
+System.out.print(largest);
+}
+
+public static float getShortestDist (String path){
+    int y = 0 ; 
+    int x = 0 ;
+
+    for (int i =0 ; i<path.length() ; i++){
+        char dir = path.charAt(i);
+        if(dir == 'S'){
+            y--;
+        } else if (dir =='N'){
+            y++;
+        } else if (dir == 'E'){
+            x++;
+        } else if (dir == 'W'){
+            x--;
+        }
+    }
+
+    return (float)Math.sqrt(x*x + y*y);
+}
 
     public static boolean isPalindrome(String str) {    
         for(    int i = 0; i < str.length() / 2; i++) {
