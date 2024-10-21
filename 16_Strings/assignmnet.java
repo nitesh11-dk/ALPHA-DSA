c3d4720ebc4079a0cf53f6340a1b748b464d1cc8
Nitesh-DK <nittesh.dk@gmail.com>
2024-10-21 19:46:44 +0530

    all done

---
+++ 16_Strings/assignmnet.java
@@ -0,0 +1,8 @@
+public class assignmnet {
+
+    public static void main(String[] args) {
+//         Question 1 : Count how many times lowercase vowels occurred ina String entered by the user
+          
+
+    }
+}
