cb36bc266fd65d6efdfc03f711018ab40b331bbf
Niteshkumar Kushwaha <nittesh.dk@gmail.com>
2024-11-10 16:21:25 +0530

    Update app.java

--- 17_Bit_Manipulation/app.java
+++ 17_Bit_Manipulation/app.java
@@ -3,7 +3,7 @@ public class app {
     public static void main(String[] args) {
         // System.out.print(2 | 1);
         // System.out.print(2 & 1);
-     //TODO:   // System.out.print(~); // negation nahi hoga 2's complement ho ajayega , sign depend karta hai MSB par if 1  then negative if 0 then positive
+     //TODO:   // System.out.print(~); // negation nahi hoga 2's complement ho ajayega , sign depend karta hai MSB par if 1  then negative if 0 then positive 
 
 // ?  left shift << 
 //   a<<b  = a * 2^b
