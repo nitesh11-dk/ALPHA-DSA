57189cf327448445ce67fed54f15fe44f0888fea
Niteshkumar Kushwaha <nittesh.dk@gmail.com>
2024-11-28 19:20:06 +0530

    Update app.java

--- 14_Sorting_Algo/app.java
+++ 14_Sorting_Algo/app.java
@@ -4,8 +4,6 @@ import java.util.Collections;
 class app{
 
     public static void main(String args[]){
-
- 
 // ? Bubble sort
 // bubbleSort(new int[]{4,9,15,17,5,8});
 // bubbleSortM2(new int[]{1,2,3,4,5,6}); // also handles for average case 
