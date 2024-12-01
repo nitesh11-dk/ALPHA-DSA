be8fdda9fb63ec84c1b5dc138d1f58a81c18d10a
nitesh11-dk <nittesh.dk@gmail.com>
2024-12-01 03:16:25 +0000

    all configuration done

--- 16_Strings/assignmnet.java
+++ 16_Strings/assignmnet.java
@@ -1,8 +1,9 @@
 public class assignmnet {
 
     public static void main(String[] args) {
-//         Question 1 : Count how many times lowercase vowels occurred ina String entered by the user
-          
+        // Question 1 : Count how many times lowercase vowels occurred ina String
+        // entered by the user
+        System.out.println("Hello kese ho ");
 
     }
 }
