8955e98aaf3088f4e3b29ed3b53897f771fa2a21
Nitesh-DK <nittesh.dk@gmail.com>
2025-01-14 09:40:33 +0530

    Add new Java files for basic patterns and character printing examples

---
+++ a-dsa-reloaded/10-11/app.java
@@ -0,0 +1,38 @@
+ public class app {
+     public static void main(String[] args) {
+         
+//          for(int i = 0; i < 4; i++) {
+//  for (int j = 0; j <= i; j++) {
+//    System.out.print("*");
+//      }
+//      System.out.println();
+//          }
+     
+         
+//          for(int i = 0; i < 4; i++) {
+//  for (int j = 0; j < 4-i; j++) {
+//    System.out.print("*");
+//      }
+//      System.out.println();
+//          }
+     
+//          for(int i = 0; i < 4; i++) {
+//  for (int j = 0; j < i+1; j++) {
+//    System.out.print(j+1);
+//      }
+//      System.out.println();
+//          }
+
+// char ch = 'A';
+// for(int i = 0; i < 4; i++) {
+//  for (int j = 0; j < i+1; j++) {
+//    System.out.print(ch);
+//    ch++;
+//      }
+//      System.out.println();
+//          }
+
+     
+     
+     }
+ }
