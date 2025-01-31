7f2ad5677c581d702238f4fe31b6abc5d0339052
Nitesh-DK <nittesh.dk@gmail.com>
2025-01-31 20:36:50 +0530

    done sorting algorithms

--- DATA_STRUCTURE/14_Basic_Sorting_Algo/app.java
+++ DATA_STRUCTURE/14_Basic_Sorting_Algo/app.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Collections;
+
 public class app{
 
     public static int bubbleSort(int arr[]){
@@ -48,12 +51,36 @@ public static void insertionSort(int arr[]){
 }
 
 
+
+
+public static void  countingSort(int arr[]){
+   int largest = Integer.MIN_VALUE;
+   for(int i = 0;i<arr.length; i++){
+    largest = Math.max(largest,arr[i]);
+   }
+   int count[] =  new int[largest+1]; // staring from zero ,tf
+   for(int i = 0;i<arr.length;i++){
+    count[arr[i]]++;
+   }
+//    sorting 
+int j = 0 ;
+for(int i = 0;i<count.length;i++){
+    while(count[i] >0){
+   arr[j]= i;
+   j++;
+   count[i]--;
+    }
+}
+
+}
+
+
     public static void main (String args[]){
 
-//  * Bubble Sort
+//  ! Bubble Sort
 // large elements come to the end of array by swapping with adjacentelements
 //  if array is by deault sorted then , the in first turn only no swapping is done do return from there only 
-int arr[] =new int[]{1,2,3,20,44}; 
+int arr[] =new int[]{1,245,4}; 
 // bubbleSort(arr);
 
 // ? BUBBLE SORT ME HUM BADE ELEMENT KO LAST ME RAKHTE HAI
@@ -61,19 +88,43 @@ int arr[] =new int[]{1,2,3,20,44};
 //  ? selection me jada swaping nahi hoti hai 
 //  * selection sort me hum pahale chote element dhunde hai then yusko swap karte hai , jisase ki  am  swaping hoti hai ,ha  hum ye chiz bubble sort me vhi kar sakte hai ... 
 
-//  * sELECTION SORT 
+//  ! sELECTION SORT 
 // pick the smallest (from unsorted), put it at the beginning
 //  selectionSort(arr);
 
 
-// * Insertion sort 
+// ! Insertion sort 
 // Pick an element(from unsorted part) & place in the right pos in sorted part
-insertionSort(arr);
+// insertionSort(arr);
+
+
+// ! Java Inbuild Sorting
+// int arr2[] = new int[]{10,6,5,8,4};
+// Arrays.sort(arr2);
+// Arrays.sort(arr2,0,2); //? (arrayname , startIndex , endIndex)
+
+// * Reverse sorting is done Using Collections
+// int arr2[] = {10,6,5,8,4}; // get errore when reverse order int[] is not applicable
+// ? ReverseOrder work on Objects types not on primitive types 
+Integer arr2[] = {10,6,5,8,4}; 
+Arrays.sort(arr2,Collections.reverseOrder());
+// ?  by deafult reverseOrder uses comparator 
+
+//  ! Counnting sort 
+//  ? it is used where , the number we want to sort are not too big 
+// ? +ve , minumun range , we can say the range is small 
+int arrr[] = {2,3,5,32,6,23,52};
+ countingSort(arrr);
+//  ? agar -ve negative number huya to
+//  yunko +ve jaise treat kare , aur yunko yak naye arr me dal le  
+//  then yus arryako count sort karle then yunko decesending  me  kar le 
+// to then negative wale numbers ko +ve walo ko sat front me  concat karde then negative wale array ko -ve sign dede 
+// ? but the counting sort is done on +ve numbers only and also the range of numbers should be low no matter what is the quantity of the numbers 
 
 
 
-for(int  j: arr){
-            System.out.print(j);
+for(int  j: arrr){
+            System.out.print(j + ",");
         }
 
     }
