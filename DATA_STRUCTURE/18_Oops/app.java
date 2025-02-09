c5bcd70d9d49b258588ab99944c664e300afc324
Nitesh-DK <nittesh.dk@gmail.com>
2025-02-09 14:06:55 +0600

    done

--- DATA_STRUCTURE/18_Oops/app.java
+++ DATA_STRUCTURE/18_Oops/app.java
@@ -1,43 +1,37 @@
-public class app {
 
+public class app {
     //  * main class requeired public and static 
     public static void main(String[] args) {
-        // Creating object of class
-        Student s1 = new Student();
-        s1.name = "Rahul";
-        s1.roll_no = 101;
-        s1.marks = 90;
-        s1.display();
+        // Student s1 = new Student();
+        // s1.name = "Rahul";
+        // s1.roll_no = 101;
+        // s1.marks = 90;
+        // s1.display();
 
-        // ? bank acc
         BankAccount user1 = new BankAccount();
         user1.username = "kese ho";
         // user1.password = "1234"; // this is a private access 
         //  ? inseted we use settter method to set the pass 
         user1.setPass("hello");
-        
-
+        System.out.println(user1.getId());
     }
 }
 
 class BankAccount{
     public String username;
     private String password;
+    private String id="Nite1109";
     public void setPass(String pass){
         password = pass ;
+    }
+    String getId(){
+        return this.id;
     }
 }
 
 // ? when we create a class , it doesn't reserve the memeory , but we are creating the instance of that class , the the instance resever the memeory 
 
-class Student {
-    String name;
-    int roll_no;
-    int marks;
+// ? Encapsulation : - is defiend as the wrapping up of data and methods under a single unit. It also implements data hiding .interface
 
-    void display() {
-        System.out.println("Name: " + name);
-        System.out.println("Roll No: " + roll_no);
-        System.out.println("Marks: " + marks);
-    }
-}
+
+
---
+++ DATA_STRUCTURE/18_Oops/cons.java
@@ -0,0 +1,61 @@
+public class cons {
+
+    public static void main(String[] args) {
+        // * constructor overloading , matlb kii based on the arguments passed to the
+        // constructor, the suitable constuctor is caleed , is overloading of
+        // constructor
+        Student s1 = new Student("ayush");
+        Student s2 = new Student(12121);
+        System.out.println(s1.name);
+        System.out.println(s2.roll);
+        // * type of constructor
+        // ? parameterisized
+        // ? non-parameterisized
+        // ? constructor overloading , polymorphism
+
+        // ? copy constructor , bydeafult we dont get in java , but in c++ we get
+        
+        // ! yak obj ki sare property ko dusare obj me copy kar lena
+        Student clv = new Student("niteh");
+        clv.roll = 101;
+        clv.marks[0] =12 ;
+        clv.marks[1] =54 ;
+        clv.marks[2] =34 ;
+
+        Student clv2 = new Student(clv);
+        clv2.password = "xyz";
+        clv.marks[1] =55 ; // array ka refernce jara hai , but obj hoga to yuska vhi hoga  ;
+
+         for(int i = 0 ; i < clv2.marks.length;i++){
+            System.out.println(clv.marks[i]);
+         }
+    }
+
+}
+
+class Student {
+    String name;
+    int roll;
+    String password;
+   int marks[];
+    // ?copy constructor
+    Student(Student s1) {
+        marks = new int[3];
+        this.name = s1.name;
+        this.roll = s1.roll;
+        this.marks = s1.marks; // marks array hai so referrence copy hoga 
+    }
+
+    Student(String name) {
+        marks = new int[3];
+        this.name = name;
+        // system out println();
+    }
+
+    Student(int roll) {
+        marks = new int[3];
+        this.roll = roll;
+        // system out println();
+    }
+
+}
