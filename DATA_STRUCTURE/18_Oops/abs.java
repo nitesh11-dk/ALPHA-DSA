ca5c17269aafae6069c8866d87f526fa99d41c21
Nitesh-DK <nittesh.dk@gmail.com>
2025-02-10 16:59:31 +0600

    done with abstarction

--- DATA_STRUCTURE/18_Oops/abs.java
+++ DATA_STRUCTURE/18_Oops/abs.java
@@ -12,13 +12,23 @@ public class abs {
 //   ? abstact method is the method , in which there is no implementation 
 
  Horse h = new Horse();
- h.walk();
+//  h.walk();
 // Animal  a = new Animal() ; // error  ayega 
 //  agar class abstatract nahi hai to app yuske yander ke methods ko abstarct nahi baana satke hai 
 
 //!   can have constructors  , jab instance bana nahi satke hai to conscturcor ka payeda kya hoga 
 // h.changeColor();
 // System.out.println(h.color);
+
+
+// ! INterfaces  ( Multiple inhertiance )
+//  class is the blueprint for creating objects 
+//  interface is the blueprint for creating classes 
+//  100% abstraction and multiple inheritaance 
+//   - all methods are public , abstract and without implementation 
+//  - used to achieve toatl abstraction 
+//  _ variables in the interface are final , public and static 
+
 
     }
 }
@@ -34,6 +44,7 @@ abstract class Animal {
 }
 // ? now abb jab abstract class me abstract method bana hi liya hai , to jab ye class ko koi parent class bananta hai tab yusme walk function ka defineiton bata na padega
 class Horse extends Animal{
+  @Override
   void walk(){
     System.err.println("chal chal");
   }
