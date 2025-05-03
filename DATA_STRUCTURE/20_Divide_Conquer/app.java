7be3b2bf77b36deac7064d55c952f7c2baaf08a7
Nitesh-DK <nittesh.dk@gmail.com>
2025-05-03 09:43:38 +0530

    done quick sort also

--- DATA_STRUCTURE/20_Divide_Conquer/app.java
+++ DATA_STRUCTURE/20_Divide_Conquer/app.java
@@ -49,18 +49,53 @@ public class app {
 
     }
 
+    public static void quickSort(int arr[], int si, int ei) {
+        if (si >= ei) {
+            return;
+        }
+
+        int pIdx = partition(arr, si, ei);
+        quickSort(arr, si, pIdx - 1); // left
+        quickSort(arr, pIdx + 1, ei); // right
+    }
+
+    public static int partition(int arr[], int si, int ei) {
+        int pivot = arr[ei];
+
+        int i = si - 1; // to make place for els smaller than pivot
+
+        for (int j = si; j < ei; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                // swap
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        i++;
+        // swap
+        int temp = pivot;
+        arr[ei] = arr[i];
+        arr[i] = temp;
+
+        return i;
+    }
+
     public static void main(String[] args) {
 
         // Merge Sort : -
         // ? time comp = nlogn
-        // ? to impleement this merge sort we us depth first approach 
-        // 
+        // ? to impleement this merge sort we us depth first approach
+        //
         int arr[] = new int[] { 3, 222, 7, 3, 8, 3, 6 };
-        mergeSort(arr, 0, arr.length - 1);
-        printArray(arr);
+        // mergeSort(arr, 0, arr.length - 1);
+        // printArray(arr);
 
-        // ! quick sort 
-        
+        // ! quick sort
+        // uses pivot & partition
+        quickSort(arr, 0, arr.length - 1);
+        printArray(arr);
 
     }
 }
